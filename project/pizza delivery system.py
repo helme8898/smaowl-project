a7dbec0201c7d68799bc1ff0f2bc5c51aae9ae12
helme8898 <67007807+helme8898@users.noreply.github.com>
2020-06-16 18:55:46 +0530

    Add files via upload

---
+++ project/pizza delivery system.py
@@ -0,0 +1,35 @@
+print("here are the options for the base. thin crust,thick crust")
+print("what you thought there were gonna be more options hahaha funny")
+print("scam pizza")
+baseofpizza=input("what base do you want:")
+if(baseofpizza=="thick crust"):
+    print("k ur choice")
+else:
+    print("ur choice")
+cheese=input("what cheese do u want?")
+if(cheese=="mozzerella"):
+    print("k fine but do u want inside or outside or both ya know what idc")
+else:
+    print("k cottage cheese it is")
+pepperoni=input("do u want pepperoni or no")
+if(pepperoni=="yes i do"):
+    print("k")
+    fare=50
+else:
+    print("k,but no option for margarita pizza ok?")
+    fare=45
+veggies=input("do you want vegetables?")
+if(veggies=="yes"):
+    print("ok")
+    fare=60
+else:
+    print("we will still give u some cuz its good for you stomach")
+    fare=55
+    
+name=input("enter your name")
+location=input("enter ur address plz")
+
+print("you total fare is $",fare)
+
+
+
